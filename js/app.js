f284bd0b97d480a37e01305679ccd76cd1843f4c
Aimée Balcázar <aimee@example.com>
2025-06-20 18:56:26 +0200

    subida4

---
+++ js/app.js
@@ -0,0 +1,27 @@
+
+// Código para el menú hamburguesa
+
+document.addEventListener('DOMContentLoaded', () => {
+    const boton = document.getElementById('hamburguesa');
+    const menu = document.getElementById('menu');
+    const overlay = document.getElementById('overlay');
+
+    boton.addEventListener('click', () => {
+      menu.classList.toggle('activo');
+      overlay.classList.toggle('activo');
+    });
+
+    overlay.addEventListener('click', () => {
+      menu.classList.remove('activo');
+      overlay.classList.remove('activo');
+    });
+
+    // Cierra el menú al hacer clic en un enlace
+    const enlaces = menu.querySelectorAll('a');
+    enlaces.forEach(enlace => {
+      enlace.addEventListener('click', () => {
+        menu.classList.remove('activo');
+        overlay.classList.remove('activo');
+      });
+    });
+  });
